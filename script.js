7211c401f9900d9dc2ce1b960e59b91e0ae21f36
Saksham <sakshamchopra174@gmail.com>
2023-06-21 16:29:05 +0530

    alarm - 01

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ document.addEventListener('DOMContentLoaded', () =>
 )
 
 function updateTime() {
-  document.documentElement.style.setProperty('--timer-day', "'" + moment().format("dd") + "'");
-  document.documentElement.style.setProperty('--timer-hours', "'" + moment().format("k") + "'");
-  document.documentElement.style.setProperty('--timer-minutes', "'" + moment().format("mm") + "'");
-  document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format("ss") + "'");
+  const now = new Date();
+  const day = now.toLocaleString('en', { weekday: 'short' });
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  document.documentElement.style.setProperty('--timer-day', "'" + day + "'");
+  document.documentElement.style.setProperty('--timer-hours', "'" + hours + "'");
+  document.documentElement.style.setProperty('--timer-minutes', "'" + minutes + "'");
+  document.documentElement.style.setProperty('--timer-seconds', "'" + seconds + "'");
   requestAnimationFrame(updateTime);
 }
